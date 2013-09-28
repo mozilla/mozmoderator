a831a7a326deb91d372a53c6a1d6dcfc12085595
Nikos Roussos <nikos@roussos.cc>
2013-09-28 21:33:17 +0300

    fix maxlength for questions

--- moderator/moderate/forms.py
+++ moderator/moderate/forms.py
@@ -4,11 +4,13 @@ from models import Question
 
 Q_PLACEHOLDER = 'Ask your question in 140 characters'
 
+
 class QuestionForm(forms.ModelForm):
     """Question Form."""
     class Meta:
         model = Question
         fields = ['question']
         widgets = {
-            'question': forms.TextInput(attrs={'placeholder': Q_PLACEHOLDER})
+            'question': forms.TextInput(attrs={'placeholder': Q_PLACEHOLDER,
+                                               'maxlength': '140'})
             }
