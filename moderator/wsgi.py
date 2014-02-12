25841c899385ccc6a122d4c5951b0e950f5e8f3a
John Giannelos <johngiannelos@gmail.com>
2014-02-12 21:56:30 +0200

    Setup deployment for stackato.

--- moderator/wsgi.py
+++ moderator/wsgi.py
@@ -1,23 +1,4 @@
-"""
-WSGI config for moderator project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
 import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
