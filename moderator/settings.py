f0cf7f7517072d565310af309b0e4e2941e55272
Tasos Katsoulas <akatsoulas@gmail.com>
2016-08-18 19:01:45 +0300

    Add python decouple to the project.

--- moderator/settings.py
+++ moderator/settings.py
@@ -1,7 +1,10 @@
 # This is your project's main settings file that can be committed to your
 # repo. If you need to override a setting locally, use local.py
+import json
 import os
 
+import dj_database_url
+from decouple import Csv, config
 from django_jinja.builtins import DEFAULT_EXTENSIONS
 from django_sha2 import get_password_hashers
 
@@ -10,7 +13,6 @@ path = lambda *a: os.path.abspath(os.path.join(ROOT, *a))  # noqa
 
 # Defines the views served for root URLs.
 ROOT_URLCONF = 'moderator.urls'
-DEBUG = False
 
 INSTALLED_APPS = [
     # Django apps
@@ -40,43 +42,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = path('media')
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = path('static')
-STATIC_URL = '/static/'
-
-ADMINS = (
-    # ('name', 'email'),
-)
-MANAGERS = ADMINS
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-TIME_ZONE = 'UTC'
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = False
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = False
-
-# Instruct session-csrf to always produce tokens for anonymous users
-ANON_ALWAYS = True
-
-SITE_ID = 1
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -133,6 +98,8 @@ def COMPRESS_JINJA2_GET_ENVIRONMENT():
     from django.template import engines
     return engines['jinja2'].env
 
+SITE_ID = 1
+
 # Auth
 # The first hasher in this list will be used for new passwords.
 # Any other hasher in the list can be used for existing passwords.
@@ -146,66 +113,75 @@ BASE_PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
 )
 
-HMAC_KEYS = {
-    '2015-08-17': 'pancakes',
-}
-
-PASSWORD_HASHERS = get_password_hashers(BASE_PASSWORD_HASHERS, HMAC_KEYS)
-
-
 # Django browserid authentication backend
 AUTHENTICATION_BACKENDS = (
     'moderator.moderate.backend.ModeratorBrowserIDBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
-SITE_URL = 'https://moderator.mozilla.org'
-ALLOWED_HOSTS = [SITE_URL]
-BROWSERID_AUDIENCES = [SITE_URL]
+HMAC_KEYS = config('HMAC_KEYS', cast=json.loads)
 
-# Create account for new users.
-BROWSERID_CREATE_USER = True
-BROWSERID_VERIFY_CLASS = 'moderator.moderate.views.BrowserIDVerify'
-
-# Path to redirect to on successful login.
-LOGIN_REDIRECT_URL = '/'
-# Path to redirect to on unsuccessful login attempt.
-LOGIN_REDIRECT_URL_FAILURE = '/'
-LOGOUT_REDIRECT_URL = '/'
+PASSWORD_HASHERS = get_password_hashers(BASE_PASSWORD_HASHERS, HMAC_KEYS)
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = path('media')
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = path('static')
+STATIC_URL = '/static/'
+
+# Internationalization
+TIME_ZONE = config('TIME_ZONE', default='UTC')
+USE_I18N = config('USE_I18N', default=False, cast=bool)
+USE_L10N = config('USE_L10N', default=False, cast=bool)
+USE_TZ = config('USE_TZ', default=True, cast=bool)
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+SITE_URL = config('SITE_URL')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+# Instruct session-csrf to always produce tokens for anonymous users
+ANON_ALWAYS = config('ANON_ALWAYS', default=True, cast=bool)
+
+# Create account for new users.
+BROWSERID_CREATE_USER = config('BROWSERID_CREATE_USER', default=True, cast=bool)
+BROWSERID_VERIFY_CLASS = 'moderator.moderate.views.BrowserIDVerify'
+BROWSERID_AUDIENCES = config('BROWSERID_AUDIENCES', cast=Csv())
+
+# Path to redirect to on successful login.
+LOGIN_REDIRECT_URL = config('LOGIN_REDIRECT_URL', default='/')
+# Path to redirect to on unsuccessful login attempt.
+LOGIN_REDIRECT_URL_FAILURE = config('LOGIN_REDIRECT_URL_FAILURE', default='/')
+LOGOUT_REDIRECT_URL = config('LOGOUT_REDIRECT_URL', default='/')
+
 # Mozillians API
-MOZILLIANS_API_BASE = "https://mozillians.org/api/v1/users/"
+MOZILLIANS_API_BASE = config('MOZILLIANS_API_BASE')
+MOZILLIANS_API_KEY = config('MOZILLIANS_API_KEY', default='')
+MOZILLIANS_API_APPNAME = config('MOZILLIANS_API_APPNAME', default='')
 
 # Paginator items per page
-ITEMS_PER_PAGE = 10
+ITEMS_PER_PAGE = config('ITEMS_PER_PAGE', default=10, cast=int)
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
+# Sessions
+SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', default=True, cast=bool)
+
+# Database
+DATABASES = {
+    'default': config('DATABASE_URL', cast=dj_database_url.parse)
 }
+
+# Enable debugging only if in dev env
+if DEBUG:
+    for backend in TEMPLATES:
+        backend['OPTIONS']['debug'] = DEBUG
--- moderator/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import *  # noqa
-
-try:
-    from .local import *  # noqa
-except ImportError as exc:
-    exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
-    raise exc
