143374bdcddc16c505dc776772b8907aa091af64
John Giannelos <johngiannelos@gmail.com>
2016-08-22 17:01:31 +0300

    Fix django-session-csrf monkeypatching.

--- moderator/urls.py
+++ moderator/urls.py
@@ -1,7 +1,12 @@
+import session_csrf
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+session_csrf.monkeypatch()
 
 
 urlpatterns = [
