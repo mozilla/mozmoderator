bd9456c9400d8f01ddf3c54868c0e955c36acdfc
John Giannelos <johngiannelos@gmail.com>
2016-08-22 16:55:42 +0300

    Upgrade django to latest 1.9x version.

--- moderator/urls.py
+++ moderator/urls.py
@@ -1,17 +1,17 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = patterns(
-    '',
+
+urlpatterns = [
     # Admin
     url(r'^admin/', include(admin.site.urls)),
     # BrowserId
     url(r'', include('django_browserid.urls')),
     # Main landing page
     url(r'^', include('moderator.moderate.moderate_urls')),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
