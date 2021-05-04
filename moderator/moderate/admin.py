c7cef5b3d31b214a4ca0b1dc5aa66634417fbe23
Tasos Katsoulas <akatsoulas@gmail.com>
2021-05-04 16:26:00 +0300

    Use select2 autocomplete in admin.

--- moderator/moderate/admin.py
+++ moderator/moderate/admin.py
@@ -1,13 +1,12 @@
 import csv
 
-from django.db.models import Count
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.db.models import Count
 from django.http import HttpResponse
 
-from moderator.moderate.models import Event, MozillianProfile, Vote, Question
-
+from moderator.moderate.models import Event, MozillianProfile, Question, Vote
 
 # Unregister User from admin to attach MozillianProfile
 admin.site.unregister(User)
@@ -15,53 +14,76 @@ admin.site.unregister(User)
 
 def export_questions_csv(modeladmin, request, queryset):
     """Export questions csv."""
-    filename = 'questions.csv'
-    response = HttpResponse(mimetype='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="%s"' % filename
+    filename = "questions.csv"
+    response = HttpResponse(mimetype="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="%s"' % filename
 
     writer = csv.writer(response)
 
     for e in queryset:
         writer.writerow([e.name])
-        questions = (e.questions
-                     .annotate(vote_count=Count('votes'))
-                     .order_by('-vote_count'))
+        questions = e.questions.annotate(vote_count=Count("votes")).order_by(
+            "-vote_count"
+        )
         for q in questions:
-            writer.writerow([q.question.encode('utf-8'), q.votes.count()])
+            writer.writerow([q.question.encode("utf-8"), q.votes.count()])
 
     return response
 
 
 class MozillianProfileInline(admin.StackedInline):
     model = MozillianProfile
-    fk_name = 'user'
+    fk_name = "user"
 
 
 class UserAdmin(UserAdmin):
     inlines = [MozillianProfileInline]
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_nda_member', 'is_staff',)
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_nda_member",
+        "is_staff",
+    )
+    search_fields = ["email", "first_name", "last_name"]
 
     def is_nda_member(self, obj):
         return obj.userprofile.is_nda_member
+
     is_nda_member.boolean = True
 
 
 class QuestionInline(admin.StackedInline):
     model = Question
-    fields = ('addressed', 'question', 'asked_by',)
-    readonly_fields = ('question', 'asked_by',)
+    fields = (
+        "addressed",
+        "question",
+        "asked_by",
+    )
+    readonly_fields = (
+        "question",
+        "asked_by",
+    )
     extra = 0
 
 
 class EventAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
-    list_display = ('name', 'questions_count', 'created_at', 'archived', 'is_nda',)
+    list_display = (
+        "name",
+        "questions_count",
+        "created_at",
+        "archived",
+        "is_nda",
+    )
     actions = [export_questions_csv]
-    list_filter = ['archived']
+    list_filter = ["archived"]
+    autocomplete_fields = ["moderators"]
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'asked_by', 'event')
+    list_display = ("id", "asked_by", "event")
 
 
 admin.site.register(User, UserAdmin)
