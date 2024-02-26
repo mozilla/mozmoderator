6701c029a1c28cc6cf75af784160b2c3893fdc65
Tasos Katsoulas <akatsoulas@gmail.com>
2024-02-26 21:05:27 +0200

    Allow event moderators to reply

--- moderator/moderate/templatetags/helpers.py
+++ moderator/moderate/templatetags/helpers.py
@@ -14,3 +14,9 @@ def to_markdown(text):
     """Render markdown text to HTML."""
     md = markdown.Markdown(extensions=["fenced_code", "pymdownx.tilde"])
     return mark_safe(md.convert(text))
+
+
+@library.global_function
+def can_answer_question(question, user):
+    """Check if a user can answer a question."""
+    return not question.answer and question.event.moderators.filter(id=user.id).exists()
